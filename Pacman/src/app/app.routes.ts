52fd626325f290034799f1c0d93fe1e9cc31ef57
HugSTapG <hugo.tapia@uees.edu.ec>
2025-02-18 17:43:02 -0500

    Refactor_Pacman

--- Pacman/src/app/app.routes.ts
+++ Pacman/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MenuComponent, PacmanComponent } from './components/index.components';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MenuComponent
+    },
+    {
+        path: 'pacman',
+        component: PacmanComponent
+    }
+];
+
